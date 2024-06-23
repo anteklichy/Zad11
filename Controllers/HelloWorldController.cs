37c40a35ea793a61b7dc9ed965723ff40fc3ea88
anteklichy <antek.lichy@gmail.com>
2024-06-23 22:05:56 +0200

    Dodaj pliki projektów.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using Zad11.Controllers;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    private readonly ILogger<HelloWorldController> _logger;
+
+    public HelloWorldController(ILogger<HelloWorldController> logger)
+    {
+        _logger = logger;
+    }
+
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zad11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
